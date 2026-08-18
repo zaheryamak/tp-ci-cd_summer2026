23e833c673f07519744e445370a23b7233b4512c
zaher yamak <zaheryamak@gmail.com>
2026-08-18 11:16:59 +0300

    add to new files

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
